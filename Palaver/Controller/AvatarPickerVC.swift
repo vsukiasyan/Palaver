ca66daf4b8ae975dda39c40f7cd852a2b7e0bd4f
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-24 15:08:47 -0700

    Initial code and VC created for picking the user avatar.

---
+++ Palaver/Controller/AvatarPickerVC.swift
@@ -0,0 +1,47 @@
+//
+//  AvatarPickerVC.swift
+//  Palaver
+//
+//  Created by Vic Sukiasyan on 4/24/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            return cell
+        }
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
+    @IBAction func backPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Palaver/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Palaver
+//
+//  Created by Vic Sukiasyan on 4/24/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
