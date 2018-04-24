4346089b6fb905e2262c6e952af163fd94285bfb
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-24 14:40:18 -0700

    API section of user creation pretty much complete. Needs some minor tweaks so it'll show a logged in state instead of just dismissing the VC.

--- Palaver/Controller/CreateAccountVC.swift
+++ Palaver/Controller/CreateAccountVC.swift
@@ -16,6 +16,8 @@ class CreateAccountVC: UIViewController {
     
     @IBOutlet weak var userImg: UIImageView!
     
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,7 @@ class CreateAccountVC: UIViewController {
     }
 
     @IBAction func createAccountPressed(_ sender: UIButton) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else { return }
         guard let email = emailTxt.text, emailTxt.text != "" else { return }
         guard let pass = passTxt.text , passTxt.text != "" else { return }
         
@@ -31,7 +34,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged in user", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
