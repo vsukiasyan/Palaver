d7e64526d3792f8fa90a327a40eb90213ca85d7d
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-25 15:10:41 -0700

    Hooked up the table view to display the channels available.

---
+++ Palaver/View/ChannelCell.swift
@@ -0,0 +1,37 @@
+//
+//  ChannelCell.swift
+//  Palaver
+//
+//  Created by Vic Sukiasyan on 4/25/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    @IBOutlet weak var channelName: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    
+    }
+    
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+
+}
